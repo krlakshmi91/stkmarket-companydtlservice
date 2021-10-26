19990ebf72ca3f9e978586b668111fd7f4942112
Rajalakshmi Krishnan <krlakshmi91@yahoo.com>
2021-10-26 11:39:35 +0530

    Initial commit - Company details

---
+++ src/main/java/com/stkmrkt/companydetails/CompanydetailsApplication.java
@@ -0,0 +1,13 @@
+package com.stkmrkt.companydetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanydetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanydetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stkmrkt/companydetails/CompanydetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stkmrkt.companydetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanydetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
